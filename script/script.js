cf4cf26fb891e9c17c2a8943c17cb36921a7e9ac
Thezero <67761713+Thezero27@users.noreply.github.com>
2021-08-30 15:00:17 -0500

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -9,7 +9,7 @@ const getValueInput = () =>{
         //var aviso = document.getElementById("error-login")
         document.getElementById("error-login1").innerHTML = "Login correcto";
         20200
-        open("/HACKER/Hack/ANONYMOUS/Thezero/test/index2.html")
+        open("/index2.html")
     }
     if(usuario!=usrot){
         //var aviso = document.getElementById("error-login")
